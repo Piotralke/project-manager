4d936970c42a5e6d9ef6646f2481afff1d1be40c
Piotr Dziewiecki <piotreq_46@wp.pl>
2023-08-08 20:37:06 +0200

    Navbar animation changes

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -9,38 +9,22 @@ export default function Layout() {
   const menuCollapsed = useSelector((state) => state.menuCollapsed);
 
   return (
-    <div className="flex min-h-screen">
-      {/* Pasek nawigacji */}
-      <div className={`${menuCollapsed?"w-1/12":"w-1/6"} sticky top-0 h-screen bg-gray-900 transition-all ease-linear duration-200`}>
-        <Navigation></Navigation>
-      </div>
+    <div className="flex flex-col min-h-screen"> 
+      <div className="flex flex-row">
+        {/* Pasek nawigacji */}
+        <div
+          className={`${
+            menuCollapsed ? "w-1/12" : "w-1/6"
+          } sticky top-0 h-screen bg-gray-900 transition-all ease-linear duration-200`}
+        >
+          <Navigation></Navigation>
+        </div>
 
-      {/* Kontent strony */}
-      <div className="flex-grow">
-        <Outlet></Outlet>
+        {/* Kontent strony */}
+        <div className="flex-grow">
+          <Outlet></Outlet>
+        </div>
       </div>
     </div>
-
-    // <div className="flex flex-col min-w-full min-h-screen justify-stretch">
-    //   <div className="sticky top-0 left-0 z-10 flex w-full p-3 space-x-5 bg-gray-900 space-between row">
-    //     <Link to="/home" className="text-xl font-bold text-white">
-    //       Project manager
-    //     </Link>
-    //     <div className="w-1/12">
-    //       <Select label="Select project" color="amber">
-    //         <Option>Projekt 1</Option>
-    //         <Option>Projekt 2</Option>
-    //         <Option>Projekt 3</Option>
-    //       </Select>
-    //     </div>
-    //   </div>
-    //   <div className="flex flex-row flex-grow w-full min-h-full">
-    //   <Navigation></Navigation>
-    //   <Outlet></Outlet>
-    //   </div>
-    //   <div className="h-10 text-center text-white bg-gray-900 basis-1/12">
-    //     © Piotr Dziewięcki 2023
-    //   </div>
-    // </div>
   );
 }
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -4,26 +4,23 @@ import { BiArrowToRight, BiArrowToLeft } from "react-icons/bi";
 import { IoHome } from "react-icons/io5";
 import { FaCalendarDays } from "react-icons/fa6";
 import { useSelector, useDispatch } from "react-redux";
+import { Select, Option } from "@material-tailwind/react";
 import { toggleMenu } from "../Reducers/menuReducer"; // Importuj akcję kreatora
 function NavigationItem(props) {
   return (
-    <Link className="flex items-center group" to={props.link}>
-      {props.collapsed ? (
-        <props.icon className="flex items-center justify-center w-8 h-8 text-white group-hover:text-amber-500 group-hover:cursor-pointer"></props.icon>
-      ) : (
-        <>
-          <props.icon className="w-8 h-8 text-white group-hover:text-amber-500 group-hover:cursor-pointer" />
+    <Link className="flex items-center group " to={props.link}>
+          <props.icon className="w-8 h-8 text-white transition-transform duration-100 ease-linear group-hover:text-amber-500 group-hover:scale-125 group-hover:cursor-pointer group-hover:animate-pulse" />
+          
+          
           <div
-            className={`ml-2 transition-opacity duration-200 ease-in-out ${
+            className={`ml-2 transition-opacity duration-500 ease-in-out ${
               !props.collapsed ? "opacity-100 max-w-full" : "opacity-0 max-w-0"
             }`}
           >
             <div className="font-bold text-white truncate text-md group-hover:cursor-pointer group-hover:text-amber-500">
               {props.name}
             </div>
-          </div>
-        </>
-      )}
+          </div>      
     </Link>
   );
 }
@@ -44,22 +41,23 @@ export default function Navigation() {
     <div
       className={`w-full flex flex-col justify-between min-h-full bg-gray-800 ${
         menuCollapsed ? "basis-1/12" : "basis-1/6"
-      } transition-all duration-200 `}
+      } transition duration-700 `}
     >
       <div
-        className={`menuCollapsed ? "basis-1/12" : "basis-1/6" flex flex-col w-full h-full p-5 space-y-5`}
+        className={`menuCollapsed ? "basis-1/12" : "basis-1/6" flex flex-col w-full h-full p-5 space-y-5 transition-all duration-200 ${menuCollapsed?"items-center" : "items-start"}`}
       >
         {menuCollapsed ? (
           <BiArrowToRight
-            className="w-10 h-10 text-white hover:text-amber-500 hover:cursor-pointer hover:animate-pulse"
+            className="w-10 h-10 text-white transition-transform ease-linear hover:text-amber-500 hover:cursor-pointer hover:animate-pulse hover:scale-125"
             onClick={handleToggleMenu}
           ></BiArrowToRight>
         ) : (
           <BiArrowToLeft
-            className="w-10 h-10 text-white hover:text-amber-500 hover:cursor-pointer hover:animate-pulse"
+            className="w-10 h-10 text-white transition-transform ease-linear hover:text-amber-500 hover:cursor-pointer hover:animate-pulse hover:scale-125"
             onClick={handleToggleMenu}
           ></BiArrowToLeft>
         )}
+        
         <div className="h-0.5 w-full bg-gray-400"></div>
         <NavigationItem
           icon={IoHome}
