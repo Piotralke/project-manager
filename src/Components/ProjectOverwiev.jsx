9af6438a0953e634831cddc26dcbfc67d7b44f03
Peter Dz <piotreq_46@wp.pl>
2023-11-23 14:12:52 +0100

    MainPage

--- src/Components/ProjectOverwiev.jsx
+++ src/Components/ProjectOverwiev.jsx
@@ -1,46 +1,65 @@
 import { useState } from "react";
 import { AiOutlineSnippets } from "react-icons/ai";
 import { Typography } from "@material-tailwind/react";
-
+import { FaArrowRight,FaRegEyeSlash  } from "react-icons/fa";
+import { FaEarthAfrica } from "react-icons/fa6";
 export default function ProjectOverview() {
-  const [projectData, setProjectData] = useState({
-    uuid: "123-321-123",
-    title: "Project-Manager",
-    description: "System do zarządzania projektami studenckimi",
-    status: "STARTED",
-    createdAt: "2023-10-26 15:16:40.942694+02",
-    isPrivate: false,
-  });
+    const [projectData, setProjectData] = useState({
+        uuid: "123-321-123",
+        title: "Project-Manager",
+        description: "System do zarządzania projektami studenckimi",
+        status: "STARTED",
+        createdAt: "2023-10-26 15:16:40.942694+02",
+        isPrivate: false,
+    });
 
-  const avatarUrl = "https://i.pravatar.cc/300";
+    const avatarUrl = "https://i.pravatar.cc/300";
 
-  return (
-    <div className="flex flex-row w-full h-full p-2">
-      <section className="flex basis-1/2">
-        <div className="flex flex-col">
-          <div className="flex flex-row mb-2">
-            <div className="flex flex-col">
-              <Typography variant="h5">{projectData.title}</Typography>
-              <Typography variant="small">{projectData.description}</Typography>
-            </div>
-          </div>
-          <div className="flex flex-row">
-            <div className="flex">
-              {Array.from({ length: 5 }, (_, index) => (
-                <div
-                  key={index}
-                  className="w-10 h-10 bg-cover bg-center rounded-full border-2 border-black -ml-2 relative z-10"
-                  style={{
-                    backgroundImage: `url(${avatarUrl})`,
-                    transform: `translateX(${index * -6}px)`,
-                  }}
-                ></div>
-              ))}
-            </div>
-          </div>
+    return (
+        <div className="flex flex-row w-full h-full p-3 border border-gray-400 rounded-xl ">
+            <section className="flex basis-1/2">
+                <div className="flex flex-col">
+                    <div className="flex flex-row mb-2">
+                        <div className="flex flex-col">
+                            <Typography variant="h5">{projectData.title}</Typography>
+                            <Typography variant="small">{projectData.description}</Typography>
+                        </div>
+                    </div>
+                    <div className="flex flex-row">
+                        <div className="flex">
+                            {Array.from({ length: 5 }, (_, index) => (
+                                <div
+                                    key={index}
+                                    className="w-10 h-10 bg-cover bg-center rounded-full border-2 border-black -ml-2 relative z-10"
+                                    style={{
+                                        backgroundImage: `url(${avatarUrl})`,
+                                        transform: `translateX(${index * -6}px)`,
+                                    }}
+                                ></div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="flex basis-5/12">
+                <div className="flex flex-col">
+                    <div className="flex-grow">TUTAJ BEDZIE POWIADOMIENEI O WYDARZENIU CYZ COS</div>
+                    {projectData.isPrivate ? 
+                    <div className="flex flex-row ml-auto">
+                        <Typography variant="paragraph">Prywatny</Typography>
+                        <FaRegEyeSlash className="w-5 h-5"></FaRegEyeSlash>
+                    </div> 
+                    :
+                    <div className="flex flex-row ml-auto space-x-2">
+                        <Typography variant="paragraph">Publiczny</Typography>
+                        <FaEarthAfrica className="w-5 h-5"></FaEarthAfrica>
+                    </div> 
+                    }
+                </div>
+            </section>
+            <section className="flex basis-1/12 justify-center items-center h-full">
+                <FaArrowRight className="w-10 h-10 m-atuo"></FaArrowRight>
+            </section>
         </div>
-      </section>
-      <section className="flex basis-1/2"></section>
-    </div>
-  );
+    );
 }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,15 +1,41 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Select, Option, Typography, CardHeader, CardBody, CardFooter } from "@material-tailwind/react";
 import { Card } from "@material-tailwind/react";
 import ClockCard from "../Components/ClockCard";
 import { FaCalendarAlt } from "react-icons/fa";
 import { MdPushPin } from "react-icons/md";
 import { BsStack } from "react-icons/bs";
-
+import { FaArrowRight, FaEllipsisH } from "react-icons/fa";
 import Calendar from "../Components/YearlyCalendar";
 import ProjectOverwiev from "../Components/ProjectOverwiev";
 
 export default function HomePage() {
+
+    const [userProjects, setUserProjects] = useState([{
+        uuid: "123-321-123",
+        title: "Project-Manager",
+        description: "System do zarządzania projektami studenckimi",
+        status: "STARTED",
+        createdAt: "2023-10-26 15:16:40.942694+02",
+        isPrivate: false,
+    },
+    {
+        uuid: "123-321-123",
+        title: "Project-Manager2",
+        description: "System do zarządzania projektami studenckimi huhu hehe",
+        status: "STARTED",
+        createdAt: "2023-10-26 15:16:40.942694+02",
+        isPrivate: true,
+    },
+    {
+        uuid: "123-321-123",
+        title: "Project-Manager3",
+        description: "System do zarządzania projektami studenckimi huhu hehe",
+        status: "STARTED",
+        createdAt: "2023-10-26 15:16:40.942694+02",
+        isPrivate: true,
+    }]);
+
     return (
         <div className="grid w-full h-full grid-cols-1 lg:grid-cols-5 gap-5 p-5 bg-gray-300 lg:grid-rows-8 grid-rows ">
             <section className="flex w-full col-span-full">
@@ -29,23 +55,44 @@ export default function HomePage() {
             </Card>
             <ClockCard className="col-span-full lg:col-span-2"></ClockCard>
             <Card className="col-span-full lg:col-span-2 row-span-2">
-                <CardBody className="flex flex-col p-2 w-full">
+                <CardBody className="flex flex-col p-2 w-full h-full">
                     <div className="flex flex-row space-x-4 place-content-center py-2">
-                        <MdPushPin className="w-6 h-full" />
+                        <MdPushPin className="w-6 h-6" />
                         <Typography variant="h6">Przypięty projekt</Typography>
                     </div>
                     <ProjectOverwiev />
                 </CardBody>
             </Card>
             <Card className="col-span-full lg:col-span-2 row-span-4">
-                <CardHeader className="flex flex-row space-x-4 place-content-center">
-                    <BsStack className="w-8 h-8" />
-                    <Typography variant="h3">Projekty</Typography>
-                </CardHeader>
                 <CardBody>
-
+                    <div className="flex flex-row space-x-4 place-content-center py-2">
+                        <BsStack className="w-6 h-6" />
+                        <Typography variant="h6">Projekty</Typography>
+                    </div>
+                    <div className="border border-gray-400 rounded-xl w-full overflow-hidden divide-y divide-gray-400 my-4">
+                        {userProjects.map((project, index) => (
+                            <Fragment key={index}>
+                                <div
+                                    className={`p-4 flex flex-row space-x-2 odd:bg-gray-200 `}
+                                >
+                                    <Typography variant="h5" className="font-bold mb-2 basis-5/12">
+                                        {project.title}
+                                    </Typography>
+                                    <Typography variant="p" className="basis-1/2">{project.description}</Typography>
+                                    <div className="flex items-center justify-end m-auto basis-1/12">
+                                        <FaArrowRight className="w-6 h-6" />
+                                    </div>
+                                </div>
+                            </Fragment>
+                        ))}
+                        <div className="flex flex-grow flex-row space-x-4 place-content-center items-center justify-center py-2">
+                            <FaEllipsisH className="w-6 h-6" />
+                            <Typography variant="h6">Wszystkie projekty</Typography>
+                        </div>
+                    </div>
                 </CardBody>
             </Card>
+
         </div>
     )
 }
